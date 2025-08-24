67e50bf5d6d153140d611920c2d59a9626172820
manan.pandya <manan.pandya@globallogic.com>
2025-08-24 14:44:26 +0530

    Added CORS

--- signup/signup/src/main/java/com/automotive/signup/controller/SignUpServiceController.java
+++ signup/signup/src/main/java/com/automotive/signup/controller/SignUpServiceController.java
@@ -10,15 +10,18 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/signup")
+@CrossOrigin(
+        origins = "http://localhost:3000", // React app origin
+        allowedHeaders = "*",
+        methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS},
+        allowCredentials = "true"
+)
 public class SignUpServiceController {
 
     private static final Logger logger = Logger.getLogger(SignUpServiceController.class.getName());
